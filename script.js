a5dc7a507e4d29f375589dddd8a14db635d19daa
godtremble95 <116137971+godtremble95@users.noreply.github.com>
2023-01-10 00:38:35 -0500

    Grid resizes in the range of 1x1 through 100x100 Minor styling changes

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
+const resizeBtn = document.querySelector('button');
 const container = document.querySelector('.container');
 const divGrid = [];
 
-for (i = 0; i < 256; i++) {
-  divGrid[i] = document.createElement('div');
-  container.appendChild(divGrid[i]);
+resizeBtn.addEventListener('click', getDimentions);
+
+// initialize grid at 16x16
+drawGrid(16);
+
+function drawGrid(count){
+  const tempLength = divGrid.length - 1;
+  // remove old grid one <div> at a time, working down the array
+  for (i = tempLength; i >= 0; i--) {
+    container.removeChild(divGrid[i]);
+    divGrid.pop();
+  }
+  // create <div>s and add to page one at a time
+  for (i = 0; i < (count ** 2); i++) {
+    divGrid[i] = document.createElement('div');
+    divGrid[i].classList = 'unDrawn';
+    divGrid[i].style.flexBasis = `${100/count}%`;
+    divGrid[i].addEventListener('mouseover', changeColor);
+    container.appendChild(divGrid[i]);
+  }
 }
 
-divGrid.forEach(pixel => pixel.addEventListener('mouseover', changeColor));
-
 function changeColor() {
-  this.style.backgroundColor = 'black';
+  this.classList = 'drawn';
+}
+
+function getDimentions() {
+  let count = Number(prompt("Enter your desired sketch size:\nMax: 100"));
+  if (count === 0) return;
+  while (isNaN(count) || count > 100) {
+    count = Number(prompt("Enter your desired sketch size:\nMax: 100"));
+  }
+  drawGrid(count);
 }
